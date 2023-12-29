2e1e6436eae61a8fd481f1e9cbc0017067052ad3
KevinAMamaniC <64039060+KevinAMamaniC2020@users.noreply.github.com>
2023-12-28 19:12:56 -0500

    Create duplicarcCodigo.py

---
+++ duplicarcCodigo.py
@@ -0,0 +1,23 @@
+# example.py
+
+def add_numbers(a, b):
+    return a + b
+
+def multiply_numbers(c, d):
+    return c * d
+
+def add_numbers(a, b):  # Duplicate function
+    return a + b
+
+def subtract_numbers(e, f):  # Duplicate function
+    return e - f
+
+# Duplicated code block
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
+
+# Duplicated code block
+numbers = [1, 2, 3, 4, 5]
+for number in numbers:
+    print(number)
